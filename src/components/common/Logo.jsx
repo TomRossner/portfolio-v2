11562a9895802d5a729698eda636c001b8bf0b46
Tom Rossner <tomrossner2@gmail.com>
2023-06-03 00:40:01 +0300

    Update logo

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import "../../styles/logo.scss";
+import logo from "../../assets/tom.png";
 
 const Logo = () => {
     const navigate = useNavigate();
   return (
-    <div id='logo' onClick={() => navigate('/')}>tomrossner<span>.</span>dev</div>
+    <div id='logo' onClick={() => navigate('/')}>
+      <img src={logo} alt='logo'/>
+    </div>
   )
 }
 
