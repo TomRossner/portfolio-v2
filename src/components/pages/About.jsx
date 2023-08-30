c9ae4b4c8ee8a54b43ea386760882fc47cf6ab3d
Tom Rossner <tomrossner2@gmail.com>
2023-08-30 17:31:04 +0300

    Update resume and about section

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -65,9 +65,9 @@ const About = () => {
         <div id='aboutContent'>
           <div id='aboutText' className={`${theme === DARK ? 'dark' : ''}`}>
             <p>Hello, I'm Tom Rossner, a Full Stack developer with a passion for creating robust and innovative web applications. Proficient in technologies such as <span>React & Redux, Node.js, MongoDB, and Express</span>, I specialize in developing dynamic and user-friendly solutions.</p>
-            <p>With a strong focus on writing clean and maintainable code, I strive to deliver high-quality products that meet the needs of both clients and end-users. My attention to detail ensures that every aspect of the development process is thoroughly considered, resulting in seamless and efficient applications.</p>
-            <p>As a problem solver by nature, I enjoy tackling complex challenges and finding creative solutions. I thrive in fast-paced environments where I can apply my quick-learning abilities to stay up-to-date with the latest industry trends and technologies. This adaptability allows me to rapidly acquire new skills and apply them effectively to deliver outstanding results.</p>
-            <p>In my professional journey, I have successfully completed projects that demanded a diverse range of technical expertise, providing me with a well-rounded skill set. This allows me to contribute effectively to all stages of development, from conceptualization and design to implementation and deployment.</p>
+            <p>With my strong attention to detail and focus on writing clean and maintainable code, I ensure that every aspect of the development process is thoroughly considered, resulting in seamless and efficient applications.</p>
+            <p>Being a problem solver by nature, I enjoy tackling complex challenges and finding creative solutions.</p>
+            <p>In my professional journey, I have successfully completed projects that demanded a diverse range of technical expertise, providing me with a well-rounded skill set. This allows me to contribute effectively to all stages of development.</p>
             <p>I'm excited to collaborate with forward-thinking individuals and organizations to bring their ideas to life. Feel free to explore my portfolio and get in touch with me to discuss potential opportunities. Let's create something exceptional together!</p>
 
             <div className='buttons-container'>
